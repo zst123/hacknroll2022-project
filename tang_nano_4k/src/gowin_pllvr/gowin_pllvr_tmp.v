e43cf714cb8ab5089d08aaaed33a75b4689916b3
zst123 <zst123@users.noreply.github.com>
2022-01-07 18:27:03 +0800

    Add PLL settings

---
+++ tang_nano_4k/src/gowin_pllvr/gowin_pllvr_tmp.v
@@ -0,0 +1,17 @@
+//Copyright (C)2014-2021 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//GOWIN Version: V1.9.8
+//Part Number: GW1NSR-LV4CQN48PC7/I6
+//Device: GW1NSR-4C
+//Created Time: Sun Jan  2 22:14:27 2022
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_PLLVR your_instance_name(
+        .clkout(clkout_o), //output clkout
+        .clkin(clkin_i) //input clkin
+    );
+
+//--------Copy end-------------------
